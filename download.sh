faef42afc114177c0967de328089beb46a092d93
Nurdianto <nurdiantopntr@gmail.com>
2016-04-30 08:44:48 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,14 @@
-mkdir -p /home/clone/data/{ipawal,domainawal,ekstensionawal,userdbawal,passdbawal,inisialawal,domainbaru,ekstensionbaru,passmysql,inisialbaru}
+mkdir -p /home/clone/data
+touch /home/clone/data/ipawal
+touch /home/clone/data/domainawal
+touch /home/clone/data/ekstensionawal
+touch /home/clone/data/userdbawal
+touch /home/clone/data/passdbawal
+touch /home/clone/data/inisialawal
+touch /home/clone/data/domainbaru
+touch /home/clone/data/ekstensionbaru
+touch /home/clone/data/passmysql
+touch /home/clone/data/inisialbaru
 curl -L https://github.com/nurd1n/wallpaper-clone/raw/secret/start.sh -o /home/clone/start.sh
 curl -L https://github.com/nurd1n/wallpaper-clone/raw/secret/build.sh -o /home/clone/build.sh
 curl -L https://github.com/nurd1n/wallpaper-clone/raw/secret/start.txt -o /home/clone/start.txt
