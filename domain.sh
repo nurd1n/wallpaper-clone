4a5389a1b3faa9b266244ef0a879be5b21ccd933
Nurdianto <nurdiantopntr@gmail.com>
2016-04-30 08:21:41 +0700

    Create domain.sh

---
+++ domain.sh
@@ -0,0 +1,5 @@
+mkdir -p /home/clone/data
+curl -L https://github.com/nurd1n/wallpaper-clone/raw/secret/start.sh -o /home/clone/start.sh
+curl -L https://github.com/nurd1n/wallpaper-clone/raw/secret/build.sh -o /home/clone/build.sh
+curl -L https://github.com/nurd1n/wallpaper-clone/raw/secret/start.txt -o /home/clone/start.txt
+chmod 755 /home/clone/*.sh
